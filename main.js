6d4f77bc04daa487e6ee77958adc7696031260f5
wcgama <145389177+wcgama@users.noreply.github.com>
2024-09-25 11:47:08 -0300

    adada

--- main.js
+++ main.js
@@ -5,5 +5,9 @@ function soma(num1,num2) {
 function multi(num1,num2){
     return num1*num2
 }
+function areaquadrado(lado){
+    return lado**2
+}
 console.log(soma(3,4))
 console.log(multi(4,5))
+console.log(areaquadrado(5))
