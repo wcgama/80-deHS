ee92eeaac79eed87896f91cc0a424ae7daed50f0
wcgama <145389177+wcgama@users.noreply.github.com>
2024-09-25 12:12:41 -0300

    dgg

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function areaquadrado(lado){
 let media= function (num1, num2, num3){
     return (num1+num2+num3)/3
 }
-let nome=prompt("nome")
+let nome=prompt("Qual Média vc quer?")
+parseInt(prompt(media))
 document.write(nome)
 parseInt(prompt(''))
 console.log(soma(3,4))
 console.log(multi(4,5))
 console.log(areaquadrado(5))
 console.log(media(4,4,4))
-parseInt(prompt(media))
