19951f0786c92276072986339a62805357caaa78
wcgama <145389177+wcgama@users.noreply.github.com>
2024-09-25 11:27:08 -0300

    asdada

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-alert('deu certo.. Wagner 39, Luiz 24');
-function soma(num1,num2);
-return num1+num2;
+alert('deu certo.. Wagner 39, Luiz 24')
+function soma(num1,num2) {
+    return num1+num2
+}
 
-console.log(soma(3,4));
+console.log(soma(3,4))
