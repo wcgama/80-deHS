97bcc5a45aeb829e76a4b27bfe6a6c2b39dc4273
wcgama <145389177+wcgama@users.noreply.github.com>
2024-09-25 11:38:28 -0300

    qwdqdw

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ alert('deu certo.. Wagner 39, Luiz 24')
 function soma(num1,num2) {
     return num1+num2
 }
-function areaqua(num1,num2){
+function multi(num1,num2){
     return num1*num2
 }
 console.log(soma(3,4))
-console.log(areaqua(4,5))
+console.log(multi(4,5))
