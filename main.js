39f002ede9ca5e0a4f8582464c98263da4f92fe6
wcgama <145389177+wcgama@users.noreply.github.com>
2024-10-02 12:25:42 -0300

    etet

--- main.js
+++ main.js
@@ -19,21 +19,28 @@
 // console.log(areaquadrado(5))
 // console.log(media(4,4,4))
 
-let num1 = parseInt(prompt('numero'))
-let num2 = parseInt(prompt('numero'))
-let num3 = parseInt(prompt('numero'))
-function media(x,y,z){
-    return(x+y+z)/3
+// let num1 = parseInt(prompt('numero'))
+// let num2 = parseInt(prompt('numero'))
+// let num3 = parseInt(prompt('numero'))
+// function media(x,y,z){
+//     return(x+y+z)/3
+// }
+// document.write(media(num1,num2,num3))
+// let estados =['PR','SC','BH','RJ','SP','AM','RS','AC','AL','AP','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PE','PI','RN','RO','RR','SE','TO'] 
+// let i=0
+// while( i<28){
+//     console.log(i)
+//     console.log(estados[i])
+//     i=i+1
+// }
+// for(let i=0; estados.length; i++){
+//     console.log(i)
+//     console.log(estados[i])
+// }
+let idade = ('digite sua idade')
+if(idade>17){
+    console.log('Pode Dirigir')
 }
-document.write(media(num1,num2,num3))
-let estados =['PR','SC','BH','RJ','SP','AM','RS','AC','AL','AP','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PE','PI','RN','RO','RR','SE','TO'] 
-let i=0
-while( i<28){
-    console.log(i)
-    console.log(estados[i])
-    i=i+1
-}
-for(let i=0; estados.length; i++){
-    console.log(i)
-    console.log(estados[i])
+else{
+    console.log('Ilegal Dirigir')
 }
